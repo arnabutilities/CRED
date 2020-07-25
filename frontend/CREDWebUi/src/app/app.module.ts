2bdeb7d385aac652ff82d1cae84264205507b6d6
Arnab Chaudhuri <arnab.jis.it@gmail.com>
2020-07-26 03:01:12 +0530

    User Management Component added
    this ui component will create edit add and delete users

--- frontend/CREDWebUi/src/app/app.module.ts
+++ frontend/CREDWebUi/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageUsersComponent
   ],
   imports: [
     BrowserModule,
