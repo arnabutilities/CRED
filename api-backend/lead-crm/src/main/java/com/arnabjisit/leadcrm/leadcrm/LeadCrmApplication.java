cc469b2484f89f39b6753f4964447dbfb479f3ad
Arnab Chaudhuri <arnab.jis.it@gmail.com>
2020-07-26 01:41:06 +0530

    springboot template with JPA added

---
+++ api-backend/lead-crm/src/main/java/com/arnabjisit/leadcrm/leadcrm/LeadCrmApplication.java
@@ -0,0 +1,13 @@
+package com.arnabjisit.leadcrm.leadcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadCrmApplication.class, args);
+	}
+
+}
---
+++ api-backend/lead-crm/src/test/java/com/arnabjisit/leadcrm/leadcrm/LeadCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnabjisit.leadcrm.leadcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
