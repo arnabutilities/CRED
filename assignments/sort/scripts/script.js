6e80a16d5a32e3fc0c04ea67217522e51ef3c8f8
Arnab Chaudhuri <arnab.jis.it@gmail.com>
2020-08-01 16:10:35 +0530

    first commit

---
+++ assignments/sort/scripts/script.js
@@ -0,0 +1,65 @@
+function getIntVal(e) {
+    return parseInt(e.querySelector(".value").innerText);
+}
+
+function merge(leftSet, rightSet) {
+    var sortedArr = [];
+    while (leftSet.length > 0 && rightSet.length > 0) {
+        if (getIntVal(leftSet[0]) <= getIntVal(rightSet[0])) {
+            sortedArr.push(leftSet.shift());
+        } else {
+            sortedArr.push(rightSet.shift());
+        }
+    }
+    while (leftSet.length)
+        sortedArr.push(leftSet.shift());
+    while (rightSet.length)
+        sortedArr.push(rightSet.shift());
+    return sortedArr;
+}
+function sort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    else {
+        var midpoint = parseInt(arr.length / 2);
+        var leftArr = arr.slice(0, midpoint);
+        var rightArr = arr.slice(midpoint, arr.length);
+        return merge(sort(leftArr), sort(rightArr));
+    }
+}
+
+let suffleArr = (arr) => {
+    let suffledArr = [];
+    let suffledIndex = [];
+    while (suffledArr.length < arr.length) {
+        let n = Math.floor(Math.random() * arr.length);
+        if (suffledIndex.indexOf(n) === -1) {
+            suffledArr.push(arr[n]);
+            suffledIndex.push(n);
+        }
+    }
+    return suffledArr;
+}
+
+window.onload = (ev) => {
+    var htmlCollectionObject = document.querySelectorAll(".position");
+    const fragment = document.createDocumentFragment();
+
+    document.querySelector("#shuffle").addEventListener("click", (e) => {
+        var suffledArr = suffleArr([...htmlCollectionObject]);
+        suffledArr.map((e, i) => {
+            fragment.appendChild(e);
+        });
+        document.querySelector('ul').appendChild(fragment);
+    });
+
+    document.querySelector("#sort").addEventListener("click", (e) => {
+        var sortedArr = sort([...htmlCollectionObject]);
+        sortedArr.map((e, i) => {
+            fragment.appendChild(e);
+        });
+        document.querySelector('ul').appendChild(fragment);
+    });
+};
+
